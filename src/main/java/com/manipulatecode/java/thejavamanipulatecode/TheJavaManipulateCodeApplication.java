6e55b5fe6d934bbfab333a390353112118ed092e
injinjeong <snowwwwww@naver.com>
2022-09-06 20:36:31 +0900

    first commit

---
+++ src/main/java/com/manipulatecode/java/thejavamanipulatecode/TheJavaManipulateCodeApplication.java
@@ -0,0 +1,13 @@
+package com.manipulatecode.java.thejavamanipulatecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheJavaManipulateCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheJavaManipulateCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/manipulatecode/java/thejavamanipulatecode/TheJavaManipulateCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manipulatecode.java.thejavamanipulatecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheJavaManipulateCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
